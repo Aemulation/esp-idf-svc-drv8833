fd0d890918d5692d84d8487cc2e5399188d8bc0f
Aemulation <112875887+Aemulation@users.noreply.github.com>
2025-01-15 17:44:04 +0100

    Make sleep pin not optional

--- src/sleep.rs
+++ src/sleep.rs
@@ -9,45 +9,35 @@ use crate::{single_motor::SingleMotor, Motor};
 use anyhow::Result;
 
 pub struct Sleep<'d> {
-    pin: Option<PinDriver<'d, AnyOutputPin, Output>>,
+    pin: PinDriver<'d, AnyOutputPin, Output>,
 }
 
 impl<'d> Sleep<'d> {
-    pub fn new(sleep_pin: Option<PeripheralRef<'d, AnyOutputPin>>) -> Result<Self> {
+    pub fn new(sleep_pin: PeripheralRef<'d, AnyOutputPin>) -> Result<Self> {
         Ok(Self {
-            pin: sleep_pin.map(PinDriver::output).transpose()?,
+            pin: PinDriver::output(sleep_pin)?,
         })
     }
 
     pub fn sleep(&mut self) -> Result<()> {
-        let Some(sleep_pin) = &mut self.pin else {
-            return Ok(());
-        };
-
-        if sleep_pin.is_set_high() {
-            sleep_pin.set_low()?;
-        }
+        self.pin.set_low()?;
 
         Ok(())
     }
 
     #[must_use]
     pub fn asleep(&self) -> bool {
-        self.pin.as_ref().is_none_or(PinDriver::is_set_low)
+        self.pin.is_set_low()
     }
 
     #[must_use]
     pub fn awake(&self) -> bool {
-        self.pin.as_ref().is_none_or(PinDriver::is_set_high)
+        self.pin.is_set_high()
     }
 
     pub fn wakeup(&mut self) -> Result<()> {
-        let Some(sleep_pin) = &mut self.pin else {
-            return Ok(());
-        };
-
-        if sleep_pin.is_set_low() {
-            sleep_pin.set_high()?;
+        if self.asleep() {
+            self.pin.set_high()?;
             FreeRtos::delay_ms(1);
         }
 
